3fc9a9afb601c2ec3501725d9e920dc884e94dfe
Srđan Šobat <sshobat@gmail.com>
2021-01-31 21:14:10 +0100

    Start

---
+++ main.js
@@ -0,0 +1,79 @@
+// ***** JS PRACTISING *****
+
+
+// TASK 1:
+
+// Based on object:
+
+// var someData = {
+// 	name: "Peter",
+// 	lastName: "Dinklage",
+// 	status: "married"
+// };
+
+// Create a function that should receive this object, and repack it to the new object where values from previous object represent both, keys and values of the new object.
+
+var someData = {
+	name: "Peter",
+	lastName: "Dinklage",
+	status: "married"
+};
+
+function repack(data) {
+
+
+
+
+    
+}
+
+repack(someData);
+
+
+// TASK 2: 
+
+// Based on array:
+
+// var someData = [13, 45, 56, [32, 11], 27, [55, 92]];
+
+// Create a function that should repack this array to another one where all numbers are contained in the same array, in the exact order like in provided array.
+
+
+// TASK 3: 
+
+// Based on array:
+
+// var someData = [13, 45, 56, [32, 11], 27, [55, 92]];
+
+// Create a function that should receive array, get both subarrays from parent array, and pass it into another function, which should then join those arrays. In the end result should be returned and stored in variable which contains first function.
+
+
+// TASK 4:
+
+// Based on object:
+
+// var someData = {
+// 	name: "Peter",
+// 	lastName: "Dinklage",
+// 	status: "married"
+// };
+
+// Create a function that should check if there is name in object, if yes it should return another function which should remove name from the function, if no then it should return a function which would set a name propery to the object, with the value passed once function is called.
+
+
+// TASK 5:
+
+// Based on object:
+
+// var someData = {
+// 	name: "Peter",
+// 	lastName: "Dinklage",
+// 	status: "married"
+// };
+
+// Create a method that should check if there is name in object, if yes it should create second method in the same object and then call it. The second method should remove name from the function, and console log the object in it's current state. The second method should then create a third method and call it. Third method should add name property back to the object, with value of "Mike", and then console log object.
+
+// BONUS:
+
+// Try to set name with value it had before.
+
